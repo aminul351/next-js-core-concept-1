55d2e8346969c06d2a595c21a8a2cf1b28503e47
aminul157246 <aminul.islam.157246@gmail.com>
2025-03-27 10:15:28 +0600

    navbar created

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <ul className="flex justify-center space-x-4 my-4">
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        </ul>
+    </div>
+  )
+}
